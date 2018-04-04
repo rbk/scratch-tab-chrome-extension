b7d84b3e0b20e353a66224d679299fd19753577f
Richard Keller <richard@briefmedia.com>
2018-04-04 13:07:21 -0500

    added invert

--- src/override/main.js
+++ src/override/main.js
@@ -26,6 +26,64 @@ function initCrossTabStorage() {
   });
 
 }
+
+function initInvert(){
+
+  var key = "scratchtab_invert";
+  var code = "patrick";
+  var usersCode = "";
+
+  // Set event
+  chrome.storage.local.get([key], function(result){
+    if (result[key]) {
+      document.body.classList.add('invert')
+    } else {
+      var obj = {};
+      obj[key] = 0;
+      chrome.storage.local.set(obj);
+    }
+  })
+
+  // Toggle event
+  document.addEventListener("keyup", function(e) {
+    usersCode = usersCode + e.key;
+    if (usersCode.match(code)) {
+      usersCode = "";
+      chrome.storage.local.get([key], function(result){
+        alert("Invert Colors!")
+        var obj = {}
+        obj[key] = !result[key];
+        chrome.storage.local.set(obj);
+        if (obj[key]) {
+          document.body.classList.add('invert');
+        } else {
+          document.body.classList.remove('invert');
+        }
+      })
+    }
+  });
+}
+function initHotkey(){
+  var hotkey = "9173"
+  var usrKey = "";
+  document.addEventListener("keyup", function(e) {
+    console.log(e)
+    if (e.keyCode == 91) {
+      usrKey += "91";
+    }
+    if (e.keyCode == 73) {
+      usrKey += "73"
+    }
+
+    if (usrKey.match(hotkey)) {
+      console.log('works')
+      //chrome.tabs.create();
+      usrKey = "";
+    }
+  });
+}
+initInvert();
+initHotkey();
 initCrossTabStorage();
 
 
